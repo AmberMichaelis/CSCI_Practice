369c0cf8f101bd6574ef867ca7fba4aa2b7d3d7c
Amber M. <amber.m.michaelis@gmail.com>
2025-04-04 08:43:22 -0700

    Add Sum Digits In Integer.

--- SumDigitsInInteger.java
+++ SumDigitsInInteger.java
@@ -1,22 +1,35 @@
 import java.util.Scanner;
 
 public class SumDigitsInInteger {
-    public static void main(String[] arg) {
-        Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter an integer: ");
+		int num = input.nextInt();
+		int reverse = reverse(num);
+		int sum = sumofDigits(num);
+		System.out.printf("The reverse is %d and the sum is %d.", reverse, sum);
+		input.close();
+	}
 
-        System.out.println("Enter a whole number: ");
-        int digit = scanner.nextInt();
+	public static int reverse(int number) {
+		int num = number;
+		int backwards = 0;
+		int digit = 0;
+		while (num != 0) {
+			digit = num % 10;
+			backwards = backwards * 10 + digit;
+			num /= 10;
+		}
+		return backwards;
+	}
 
-        int sum = 0;
-
-        while (digit > 0) {
-            System.out.println(digit);
-            sum += digit % 10;
-            digit /= 10;
-        }
-
-        System.out.println("The sum of the digits is " + sum + ".");
-        
-        scanner.close();
-    }
+	public static int sumofDigits(int number) {
+		int sum = 0;
+		int num = number;
+		for (int i = 0; num > 0; i++) {
+			sum += num % 10;
+			num /= 10;
+		}
+		return sum;
+	}
 }
